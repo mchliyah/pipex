e13e0eecdf0da6fe06e651a9a28246761b8311cc
Mohammed Chliyah <mchliyah@e2r1p10.1337.ma>
2022-03-02 00:43:19 +0100

    m

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: mchliyah <mchliyah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 20:05:08 by mchliyah          #+#    #+#             */
-/*   Updated: 2022/03/01 19:59:42 by mchliyah         ###   ########.fr       */
+/*   Updated: 2022/03/02 00:36:34 by mchliyah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,63 +32,50 @@ static char	*cmnd(char **path, char *cmd)
 
 int	main(int ac, char **av, char **envp)
 {
-	int		fd[2];
-	int		pid1;
-	int		pid2;
-	int		infilefd;
-	int		outfilefd;
-	char	**paths;
-	char	**args;
-	char	*cmd;
+	t_p	pip;
 
 	if (ac != 5)
 		return (0);
-	infilefd = open(av[1], O_RDONLY);
-	// open file to read from
-	if (infilefd < 0)
+	pip.infilefd = open(av[1], O_RDONLY);
+	if (pip.infilefd < 0)
 		perror ("infile error");
-	outfilefd = open(av[ac - 1], O_CREAT | O_RDWR, 0644);
-	 // create a file to write the result in 
-	if (pipe(fd) == -1)
+	pip.outfilefd = open(av[ac - 1], O_CREAT | O_RDWR, 0777);
+	if (pipe(pip.fd) == -1)
 		perror ("pip error");
 	while (ft_strncmp("PATH", *envp, 4))
 		envp++;
-	paths = ft_split(*envp, ':');
-	// commands path from envp
-	pid1 = fork();
-	  //child 1 created
-	if (pid1 < 0)
+	pip.paths = ft_split(*envp, ':');
+	pip.pid1 = fork();
+	if (pip.pid1 < 0)
 		perror ("fork1 error");
-	if (pid1 == 0)
+	if (pip.pid1 == 0)
 	{
-		dup2(fd[1], 1);
-		close(fd[0]);
-		dup2(infilefd, 0);
-		args = ft_split(av[2], ' ');
-		cmd = cmnd(paths, args[0]);
-		if (!cmd)
+		dup2(pip.fd[1], 1);
+		close(pip.fd[0]);
+		dup2(pip.infilefd, 0);
+		pip.args = ft_split(av[2], ' ');
+		pip.cmd = cmnd(pip.paths, pip.args[0]);
+		if (!pip.cmd)
 			perror("command not exist");
-		execve(cmd, args, envp);
-	 // comaand N1 must be executed and the result derected to the pip :) and the shild must be dead :( 
+		execve(pip.cmd, pip.args, envp);
 	}
-	pid2 = fork();
-	 // other child just born now hhhhh
-	if (pid2 < 0)
+	pip.pid2 = fork();
+	if (pip.pid2 < 0)
 		perror ("fork1 error");
-	if (pid2 == 0)
+	if (pip.pid2 == 0)
 	{
-		dup2(fd[0], 0);
-		close(fd[1]);
-		dup2(outfilefd, 1);
-		args = ft_split(av[3], ' ');
-		cmd = cmnd(paths, args[0]);
-		if (!cmd)
+		dup2(pip.fd[0], 0);
+		close(pip.fd[1]);
+		dup2(pip.outfilefd, 1);
+		pip.args = ft_split(av[3], ' ');
+		pip.cmd = cmnd(pip.paths, pip.args[0]);
+		if (!pip.cmd)
 			perror("command not exist");
-		execve(cmd, args, envp);
+		execve(pip.cmd, pip.args, envp);
 	}
-	close(fd[0]);
-	close(fd[1]);
+	close(pip.fd[0]);
+	close(pip.fd[1]);
 	wait(NULL);
-	waitpid(pid2, NULL, 0);
+	waitpid(pip.pid2, NULL, 0);
 	// system("leaks pipex");
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: mchliyah <mchliyah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 20:05:26 by mchliyah          #+#    #+#             */
-/*   Updated: 2022/03/01 14:43:54 by mchliyah         ###   ########.fr       */
+/*   Updated: 2022/03/01 23:27:07 by mchliyah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,22 @@
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdio.h>
-// typedef struct s_stack
-// {
-// 	char	**str;
-// 	char	*str1;
-// }	t_stack;
+
+typedef struct s_p
+{
+	int		fd[2];
+	int		pid1;
+	int		pid2;
+	int		infilefd;
+	int		outfilefd;
+	char	**paths;
+	char	**args;
+	char	*cmd;
+}	t_p;
 
 char	**ft_split(char const *s, char c);
 size_t	ft_strlen(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-
 
 #endif
