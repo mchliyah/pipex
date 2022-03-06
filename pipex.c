07a99915828efb52d5410182b1d4f54a58f49cee
mohameddayad <mohamed.chl1993@gmail.com>
2022-03-06 15:52:41 +0100

    100%

--- pipex.c
+++ pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mchliyah <mchliyah@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mchliyah <mchliyah@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 20:05:08 by mchliyah          #+#    #+#             */
-/*   Updated: 2022/03/02 00:36:34 by mchliyah         ###   ########.fr       */
+/*   Updated: 2022/03/06 15:21:37 by mchliyah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,45 @@ static char	*cmnd(char **path, char *cmd)
 	return (NULL);
 }
 
+t_p get_path(t_p pip, char **envp)
+{
+	while (ft_strncmp("PATH", *envp, 4))
+		envp++;
+	pip.paths = ft_split(*envp, ':');
+	return (pip);
+}
+t_p cmd_1(t_p pip, char **av, char **envp)
+{
+	if (pip.pid1 == 0)
+	{
+		dup2(pip.fd[1], 1);
+		close(pip.fd[0]);
+		dup2(pip.infilefd, 0);
+		pip.args = ft_split(av[2], ' ');
+		pip.cmd = cmnd(pip.paths, pip.args[0]);
+		if (!pip.cmd)
+			perror("command not exist");
+		execve(pip.cmd, pip.args, envp);
+	}
+	return (pip);
+}
+
+t_p cmd_2(t_p pip, char **av, char **envp)
+{
+	if (pip.pid2 == 0)
+	{
+		dup2(pip.fd[0], 0);
+		close(pip.fd[1]);
+		dup2(pip.outfilefd, 1);
+		pip.args = ft_split(av[3], ' ');
+		pip.cmd = cmnd(pip.paths, pip.args[0]);
+		if (!pip.cmd)
+			perror("command not exist");
+		execve(pip.cmd, pip.args, envp);
+	}
+	return (pip);
+}
+
 int	main(int ac, char **av, char **envp)
 {
 	t_p	pip;
@@ -42,40 +81,16 @@ int	main(int ac, char **av, char **envp)
 	pip.outfilefd = open(av[ac - 1], O_CREAT | O_RDWR, 0777);
 	if (pipe(pip.fd) == -1)
 		perror ("pip error");
-	while (ft_strncmp("PATH", *envp, 4))
-		envp++;
-	pip.paths = ft_split(*envp, ':');
+	pip = get_path(pip, envp);
 	pip.pid1 = fork();
 	if (pip.pid1 < 0)
 		perror ("fork1 error");
-	if (pip.pid1 == 0)
-	{
-		dup2(pip.fd[1], 1);
-		close(pip.fd[0]);
-		dup2(pip.infilefd, 0);
-		pip.args = ft_split(av[2], ' ');
-		pip.cmd = cmnd(pip.paths, pip.args[0]);
-		if (!pip.cmd)
-			perror("command not exist");
-		execve(pip.cmd, pip.args, envp);
-	}
+	pip = cmd_1(pip, av, envp);
 	pip.pid2 = fork();
 	if (pip.pid2 < 0)
 		perror ("fork1 error");
-	if (pip.pid2 == 0)
-	{
-		dup2(pip.fd[0], 0);
-		close(pip.fd[1]);
-		dup2(pip.outfilefd, 1);
-		pip.args = ft_split(av[3], ' ');
-		pip.cmd = cmnd(pip.paths, pip.args[0]);
-		if (!pip.cmd)
-			perror("command not exist");
-		execve(pip.cmd, pip.args, envp);
-	}
+	pip = cmd_2(pip, av, envp);
 	close(pip.fd[0]);
 	close(pip.fd[1]);
 	wait(NULL);
-	waitpid(pip.pid2, NULL, 0);
-	// system("leaks pipex");
 }
