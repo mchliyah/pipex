224f7a4fe0d578f17442659ee7968768f4f55219
Mohammed Chliyah <mchliyah@e2r2p8.1337.ma>
2022-03-23 02:25:06 +0100

    i don't know

--- mondatory/error.c
+++ mondatory/error.c
@@ -6,7 +6,7 @@
 /*   By: mchliyah <mchliyah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 14:05:32 by mchliyah          #+#    #+#             */
-/*   Updated: 2022/03/21 14:32:19 by mchliyah         ###   ########.fr       */
+/*   Updated: 2022/03/23 02:21:59 by mchliyah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,7 @@ int	msg(char *err)
 
 void	msg_pipe(char *arg)
 {
-	if (arg)
-		write(2, arg, ft_strlen(arg));
+	write(2, arg, ft_strlen(arg));
 	write(2, " : ", 3);
 	write(2, ERR_CMD, ft_strlen(ERR_CMD));
 	write(2, "\n", 1);
